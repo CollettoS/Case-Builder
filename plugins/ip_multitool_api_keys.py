515e5cd16e7ff5bbf508f2641c3510a55359ab5b
Alex <125412758+jenkaj-lab@users.noreply.github.com>
2025-03-01 11:49:45 +0000

    Create ip_multitool_api_keys.py

---
+++ plugins/ip_multitool_api_keys.py
@@ -0,0 +1,35 @@
+IPINFO_ACCESS_TOKEN = (
+    # https://ipinfo.io/
+    # 50,000 lookups per month
+    ""
+)
+
+ABUSE_IPDB_API_KEY = (
+    # https://www.abuseipdb.com/
+    # 1,000 lookups per day
+    ""
+)
+
+IPDATA_API_KEY = (
+    # https://ipdata.co/
+    # 1,500 lookups per day
+    ""
+)
+
+VIRUSTOTAL_API_KEY = (
+    # https://www.virustotal.com/
+    # 4 lookups per minute, 500 lookups per day, 15k lookups per month
+    ""
+)
+
+VPN_API_KEY = (
+    # https://vpnapi.io/
+    # 1,000 lookups per day
+    ""
+)
+
+PROXYCHECK_API_KEY = (
+    # https://proxycheck.io/
+    # 1,000 lookups per day
+    ""
+)
